679ed31ee2d927265fa06646dc521daf42b2775e
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-04-17 14:32:36 -0600

    Notificacion de sensores terminado

--- src/NotificationUsers/infraestructure/NotificationRouter.ts
+++ src/NotificationUsers/infraestructure/NotificationRouter.ts
@@ -1,39 +1,68 @@
 import express from "express";
-import { SendEmailUsernameController } from "./dependencies";
-import { sendEmilPasswordController } from "./dependencies";
+import {
+  SendEmailUsernameController,
+  sendEmilPasswordController,
+} from "./dependencies";
+import { ServicesMessages } from "./ServicesMessages/ServiceMessages";
+import { ServicesMessagesNotification } from "./ServicesMessages/SendMessageTemperatura";
+import { SendMessagesViolationSeguriti } from "./ServicesMessages/SendMessagesViolationSeguriti";
 import { Request, Response } from "express";
 
 export const notification = express.Router();
 
-notification.post("/putUsername", SendEmailUsernameController.run.bind(SendEmailUsernameController))
+notification.post(
+  "/putUsername",
+  SendEmailUsernameController.run.bind(SendEmailUsernameController)
+);
 
-notification.post("/putPassword", sendEmilPasswordController.run.bind(sendEmilPasswordController))
+notification.post(
+  "/putPassword",
+  sendEmilPasswordController.run.bind(sendEmilPasswordController)
+);
 
-notification.post("/",(req:Request, res:Response) =>{
-    try {
-        console.log(req.body);
-        res.status(200).send("")
-    } catch (error) {
-        res.status(500).send(error)
-    }
-    
-})
+notification.post("/temperatura", (req: Request, res: Response) => {
+  try {
+    const servicesMessages = new ServicesMessages();
+    servicesMessages.sendMessages(1, req.body);
+    res.status(200).send("");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-notification.post("/alerta-movimiento",(req:Request, res:Response) =>{
-    try {
-        console.log(req.body);
-        res.status(200).send("")
-    } catch (error) {
-        res.status(500).send(error)
-    }
-    
-})
-notification.post("/alerta-desbloqueo",(req:Request, res:Response) =>{
-    try {
-        console.log(req.body);
-        res.status(200).send("")
-    } catch (error) {
-        res.status(500).send(error)
-    }
-    
-})
+notification.post("/alerta-movimiento", (req: Request, res: Response) => {
+  try {
+    const servicesMessagesNotification = new ServicesMessagesNotification();
+    servicesMessagesNotification.run(
+      2,
+      "Alerta de posible violación seguro de tu bicicleta"
+    );
+    console.log(req.body);
+    res.status(200).send("");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+notification.post("/alerta", (req: Request, res: Response) => {
+  try {
+    const sendMessagesViolationSeguriti = new SendMessagesViolationSeguriti();
+    sendMessagesViolationSeguriti.run(
+      3,
+      "Alerta de violación al seguro de tu bicicleta atender de inmediato"
+    );
+    console.log(req.body);
+    res.status(200).send("");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+notification.post("/alerta-desbloqueo", (req: Request, res: Response) => {
+  try {
+    //console.log(req.body);
+    res.status(200).send("");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
--- src/NotificationUsers/infraestructure/ServicesMessages/SendMessagesViolationSeguriti.ts
+++ src/NotificationUsers/infraestructure/ServicesMessages/SendMessagesViolationSeguriti.ts
@@ -1,20 +1,16 @@
 import io from "socket.io-client";
-import { IWebSocket } from "../../app/services/IWebSocket";
 
-export class ServicesMessages implements IWebSocket {
-  async sendMessages(codigo: number, message: string): Promise<string> {
-    //console.log(message);
-    
+export class SendMessagesViolationSeguriti{
+  async run(codigo: number, message: string): Promise<string> {   
     try {
       const url: any = process.env.URL_API_WEBSOCKET;
       const socket = io(url);
       const data = { codigo: codigo, message: message };
       socket.on("connect", () => {
-        socket.emit("temperatura", data);
+        socket.emit("notification-alert", data);
       });
       return "Mensaje enviado";
     } catch (error) {
-      
       console.error(error);
       return "error:" + error;
     }
