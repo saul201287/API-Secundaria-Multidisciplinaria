c9cd9002eb2fd667df08558e4096fd63dd88f01d
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-04-18 03:27:41 -0600

    pruebas

--- src/NotificationUsers/infraestructure/ServicesMessages/ServiceMessages.ts
+++ src/NotificationUsers/infraestructure/ServicesMessages/ServiceMessages.ts
@@ -8,7 +8,6 @@ export class ServicesMessages implements IWebSocket {
     try {
       const url: any = process.env.URL_API_WEBSOCKET;
       const socket = io(url);
-      console.log(socket);
       
       const data = { codigo: codigo, message: message };
       socket.on("connect", () => {
