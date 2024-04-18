0f4fbf0b887db7e04fc72d945db1cd9ae66da045
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-04-18 03:31:56 -0600

    dd

--- src/NotificationUsers/infraestructure/ServicesMessages/ServiceMessages.ts
+++ src/NotificationUsers/infraestructure/ServicesMessages/ServiceMessages.ts
@@ -3,25 +3,30 @@ import { IWebSocket } from "../../app/services/IWebSocket";
 
 export class ServicesMessages implements IWebSocket {
   async sendMessages(codigo: number, message: string): Promise<string> {
-    //console.log(message);
-    
     try {
       const url: any = process.env.URL_API_WEBSOCKET;
       const socket = io(url);
-      
-      const data = { codigo: codigo, message: message };
+
+      // Manejar evento de conexión
       socket.on("connect", () => {
         console.log("Conexión establecida con el servidor de WebSocket");
-        socket.emit("temperatura", data);
+        const data = { codigo: codigo, message: message };
+        socket.emit("temperatura", data, (response: any) => {
+          // Callback de confirmación del servidor
+          console.log("Respuesta del servidor:", response);
+          socket.disconnect(); // Desconectar el socket después de enviar el mensaje
+        });
       });
+
+      // Manejar errores de conexión
       socket.on("connect_error", (error: any) => {
         console.error("Error de conexión con el servidor de WebSocket:", error);
       });
+
       return "Mensaje enviado";
     } catch (error) {
-      
-      console.error(error);
-      return "error:" + error;
+      console.error("Error al enviar mensaje:", error);
+      return "Error al enviar mensaje: " + error;
     }
   }
 }
