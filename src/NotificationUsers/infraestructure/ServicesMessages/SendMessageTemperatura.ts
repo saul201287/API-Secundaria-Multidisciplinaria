d74f4ea07e56a0772121dbc413403ba42fe1ba6c
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-04-18 01:45:51 -0600

    prueba 1.1

--- src/NotificationUsers/infraestructure/ServicesMessages/SendMessageTemperatura.ts
+++ src/NotificationUsers/infraestructure/ServicesMessages/SendMessageTemperatura.ts
@@ -7,7 +7,11 @@ export class ServicesMessagesNotification {
       const socket = io(url);
       const data = { codigo: codigo, message: message };
       socket.on("connect", () => {
+        console.log("Conexión establecida con el servidor de WebSocket");
         socket.emit("notification-alert", data);
+      });
+      socket.on("connect_error", (error: any) => {
+        console.error("Error de conexión con el servidor de WebSocket:", error);
       });
       return "Mensaje enviado";
     } catch (error) {
--- src/NotificationUsers/infraestructure/ServicesMessages/SendMessagesViolationSeguriti.ts
+++ src/NotificationUsers/infraestructure/ServicesMessages/SendMessagesViolationSeguriti.ts
@@ -5,9 +5,14 @@ export class SendMessagesViolationSeguriti{
     try {
       const url: any = process.env.URL_API_WEBSOCKET;
       const socket = io(url);
+      
       const data = { codigo: codigo, message: message };
       socket.on("connect", () => {
+        console.log("Conexión establecida con el servidor de WebSocket");
         socket.emit("notification-alert", data);
+      });
+      socket.on("connect_error", (error: any) => {
+        console.error("Error de conexión con el servidor de WebSocket:", error);
       });
       return "Mensaje enviado";
     } catch (error) {
