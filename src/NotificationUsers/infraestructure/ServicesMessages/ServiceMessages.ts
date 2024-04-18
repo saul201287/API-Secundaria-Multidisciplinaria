1e5a0664b3c628fb43bcf34543d3db20943a003d
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-04-18 03:25:42 -0600

    pruebas

--- src/NotificationUsers/infraestructure/ServicesMessages/ServiceMessages.ts
+++ src/NotificationUsers/infraestructure/ServicesMessages/ServiceMessages.ts
@@ -8,9 +8,15 @@ export class ServicesMessages implements IWebSocket {
     try {
       const url: any = process.env.URL_API_WEBSOCKET;
       const socket = io(url);
+      console.log(socket);
+      
       const data = { codigo: codigo, message: message };
       socket.on("connect", () => {
+        console.log("Conexión establecida con el servidor de WebSocket");
         socket.emit("temperatura", data);
+      });
+      socket.on("connect_error", (error: any) => {
+        console.error("Error de conexión con el servidor de WebSocket:", error);
       });
       return "Mensaje enviado";
     } catch (error) {
